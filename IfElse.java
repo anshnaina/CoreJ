19f788ba7f1523e827d74a4ff3675392a5971772
anshnaina <er.anshulnaina@gmail.com>
2021-02-09 17:58:52 +0530

    If Else

---
+++ IfElse.java
@@ -0,0 +1,15 @@
+package CoreJ;
+
+public class IfElse {
+public static void main(String[] ansh)
+{
+int a=5,b=2;
+if(a>b)
+{
+System.out.println("A is greater");
+}
+else
+
+	System.out.println("B is greater or equal to A");
+}
+}
