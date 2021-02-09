ef7cec955ee43b2c1652dcbc90a411329c359238
anshnaina <er.anshulnaina@gmail.com>
2021-02-09 18:00:36 +0530

    Max 1

---
+++ Max1.java
@@ -0,0 +1,20 @@
+package CoreJ;
+
+public class Max1 {
+public static void main(String[] ansh)
+{
+int a=4,b=1;
+if(a>b)
+{
+System.out.println("A is greater");
+}
+else  if(a<b)
+{
+System.out.println("B is greater");
+}
+else
+{
+System.out.println("A anb B are equal");	
+}
+}
+}
