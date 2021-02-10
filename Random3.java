5aa6b3c405f4f96b19df96e504bf97d94de98fa7
anshnaina <er.anshulnaina@gmail.com>
2021-02-10 14:46:08 +0530

    Math Random3

---
+++ Random3.java
@@ -0,0 +1,22 @@
+package CoreJava;
+
+public class Random3 {
+public static void main(String[] args)
+{ 
+double a= 1+ Math.random()*100;
+int v=(int)a;
+System.out.println(v);
+double b= 1+ Math.random()*100;
+int w=(int)b;
+System.out.println(w);
+double c= 1+ Math.random()*100;
+int x=(int)c;
+System.out.println(x);
+double d= 1+ Math.random()*100;
+int y=(int)d;
+System.out.println(y);
+double e= 1+ Math.random()*100;
+int z=(int)e;
+System.out.println(z);
+}
+}
