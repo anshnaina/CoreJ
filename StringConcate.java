afbeb325ac6e773a90bc0624c8374ad07283ed4b
anshnaina <er.anshulnaina@gmail.com>
2021-02-10 14:42:41 +0530

    StringConcate

---
+++ StringConcate.java
@@ -0,0 +1,14 @@
+package CoreJava;
+
+public class StringConcate 
+{
+public static void main(String[] args)
+
+{
+String a= "Ansh";
+String b= "Naina";
+String c=a.concat(b);
+System.out.println(c);
+System.out.println(c.substring(0,5));
+}
+}
